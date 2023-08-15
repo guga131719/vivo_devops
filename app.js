2a7517f4dc9d1345527bfc7646226acaec121e37
gustavomourasantos <luizgutsavomoura20@gmail.com>
2023-08-15 20:12:57 -0300

    Adicionar arquivos de projeto.

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    const name = process.env.NAME || 'Desconhecido';
+    res.end(`Olá ${name}!\n`);
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
